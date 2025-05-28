fd24a32a0c7906d7069ac6353d452eae07fe05b6
Thomas <1013596@hr.nl>
2025-05-28 13:24:52 +0200

    feat(post): added uppercase name to post

--- server/api/users/index.post.ts
+++ server/api/users/index.post.ts
@@ -1,7 +1,21 @@
 export default defineEventHandler(async (event) => {
+function titleCase(str: string) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   // Directly return the joined string
+   return splitStr.join(' '); 
+}
+
+
 const body = await readBody (event)
 const hashedpassword = await hashPassword(body.password)
 body.password = hashedpassword
+body.name = titleCase(body.name)
+console.log(body.name)
 const user = await User.create(body)
 return user.toObject()
 })
