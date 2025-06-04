cbc56ffcdd22ddc8b016a928514d462de324ee45
Thomas <1013596@hr.nl>
2025-06-04 16:14:53 +0200

    feat(components): added 2 new onboarding components

---
+++ components/activeOnboarding.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="grid grid-cols-6s gap-2 justify-items-center ">
+    <div class="col-span-4 col-start-1 text-5xl h-full"><slot name="title">Default Title</slot></div>
+    <div class="col-span-2 col-start-5 text-5xl">Challenge</div>
+</div>
+</template>
---
+++ pages/onboardingActiveTest.vue
@@ -0,0 +1,3 @@
+<template>
+    <ActiveOnboarding></ActiveOnboarding>
+</template>
